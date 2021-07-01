ea77abfb7831b34454dd81615ae555c6a1b1ba90
kantha <ballakantha504@gmail.com>
2021-07-01 16:15:55 -0700

    first commit

---
+++ src/test/java/MyProject/MavenSeleniumJava/AppiumTest.java
@@ -0,0 +1,19 @@
+package MyProject.MavenSeleniumJava;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void NativeAppAndroid()
+	{
+		System.out.println("NativeAppAndroid");
+	}
+    @Test
+    public  void IosApps()
+    {
+    	System.out.println("IosApps");
+    }
+}
---
+++ src/test/java/MyProject/MavenSeleniumJava/SeleniumTest.java
@@ -0,0 +1,20 @@
+package MyProject.MavenSeleniumJava;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	//@Test
+	public void BrowserAutomation()
+	{
+		System.out.println("BrowserAutomation");
+	}
+
+	@Test
+    public void elementsUI()
+    {
+		System.out.println("elementsUI");
+    }
+}
