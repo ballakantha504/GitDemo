572f9a8d5305dea07d2b1aae25c346fd74d7b814
kantha <ballakantha504@gmail.com>
2021-07-02 10:04:18 -0700

    this is asiantime

--- src/test/java/MyProject/MavenSeleniumJava/RestApiTest.java
+++ src/test/java/MyProject/MavenSeleniumJava/RestApiTest.java
@@ -9,7 +9,9 @@ public class RestApiTest {
 	@Test
 	public void postJira()
 	{
-		System.out.println("postJira");
+		System.out.println("postJira1");
+		System.out.println("postJira2");
+		System.out.println("postJira3");
 	}
     @Test
     public  void deleteTwitter()
